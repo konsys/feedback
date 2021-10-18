69c8d27c3af643fbec7933f89b722f69a551d345
konstantin sysuev <harry-run@yandex.ru>
2021-10-18 21:41:34 +0300

    Init commit

---
+++ src/pages/App/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import { useStore } from "effector-react";
+// import { Button } from "antd";
+
+import "./App.css";
+// import { appApi, appStore$ } from "./model/store";
+import Template from "../../ui-kit/Template";
+
+function App() {
+  // const counter = useStore(appStore$);
+  return <Template />;
+}
+
+export default App;
