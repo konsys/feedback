a3b789d8d26597565382cab6edc7ae0f595a9354
konstantin sysuev <harry-run@yandex.ru>
2021-10-18 21:43:16 +0300

    Little fix

--- src/ui-kit/Template/index.tsx
+++ src/ui-kit/Template/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Content from "../Content";
+import Footer from "../Footer";
 import Header from "../Header";
 
 export default function Template() {
   return (
     <>
       <Header />
+      <Content />
+      <Footer />
     </>
   );
 }
